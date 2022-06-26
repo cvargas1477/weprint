d7b6dd33b7120d58903c548f5c99aabf7f18f913
cvargas1477 <cvargas1477@gmail.com>
2022-06-25 22:01:40 -0400

    Modificación de clearcache para cpanel

--- routes/web.php
+++ routes/web.php
@@ -72,3 +72,17 @@ Route::post('archivos/upload',function(Request $request){
 
 })->name('archivos.upload')->middleware('auth');
 
+Route::get('/clearcache', function () {
+    $clearcache = Artisan::call('cache:clear');
+    echo "Cache cleared<br>";
+
+    $clearview = Artisan::call('view:clear');
+    echo "View cleared<br>";
+
+    $clearconfig = Artisan::call('config:cache');
+    echo "Config cleared<br>";
+
+    $cleardebugbar = Artisan::call('debugbar:clear');
+    echo "Debug Bar cleared<br>";
+});
+
